694888b05555dff4b0ac6f140786cad0e4c9b7b7
Luciano Citroni <68912098+Luciano-Citroni@users.noreply.github.com>
2023-09-19 20:01:58 -0300

    Update Data.jsx

--- src/utils/Data.jsx
+++ src/utils/Data.jsx
@@ -9,36 +9,17 @@ export const skills = [
 	},
 	{
 		id: 1,
-		tech: 'Node Js',
-		icon: <FaNode />,
-	},
-	{
-		id: 2,
-		tech:'CSS',
-		icon: <FaCss3Alt />,
-	},
-	{
-		id: 3,
 		tech:'Python',
 		icon: <FaPython />,
 	},
+
 	{
-		id: 4,
-		tech:'Javascript',
-		icon: <SiJavascript />,
-	},
-	{
-		id: 5,
+		id: 2,
 		tech:'Flutter',
 		icon: <SiFlutter />,
 	},
 	{
-		id: 6,
-		tech:'Dart',
-		icon: <SiDart />,
-	},
-	{
-		id: 7,
+		id: 3,
 		tech:'Power BI',
 		icon: <SiPowerbi />,
 	}
